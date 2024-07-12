eb5691fbd725b3fbd32bd780dfb2a96a7cae5bd9
cosmicElze <charlemagne_elliot_craw@dlsu.edu.ph>
2024-07-12 22:27:36 +0800

    final, touches on signup

--- server.js
+++ server.js
@@ -183,7 +183,6 @@ app.get('/Velasco', async (req, res) => {
 });
 /*-----------------------      SIGNUP      --------------------------*/ 
 app.post('/signup', async (req, res) => {
-    console.log(req.body + "a")
     const { fullName, email, password, title } = req.body;
 
     try { 
