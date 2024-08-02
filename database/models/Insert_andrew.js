bf14c0d46ec02a0c6c28c247949d082f041ceb44
HIEB1I <john_fredrick_tario@dlsu.edu.ph>
2024-08-02 12:32:58 +0800

    revised inserts

--- database/models/Insert_andrew.js
+++ database/models/Insert_andrew.js
@@ -5,154 +5,34 @@ mongoose.connect('mongodb+srv://johnfredricktario:password_12@tinkerlab.mbh0jj9.
 
 // run this in cmd (e.g node Insert_andrew.js)
 const sampleAndrew = [ {
-    seat: "A01",
-    reservations: [
-      {
-        name: "Mari Santos",
-        value: 1,
-        anonymous: 0,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-07-31",
-        timeofrequest: "8:00 AM",
-        timeofreservation: "7:30AM-8:00AM",
-        reservedby: "Irina Solarez"
-      }
-    ]
+    seat: "A01"
   },
   {
-    seat: "A02",
-    reservations: [
-      {
-        name: "Carlos Bautista",
-        value: 1,
-        anonymous: 0,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-07-31",
-        timeofrequest: "9:00 AM",
-        timeofreservation: "7:30AM-8:00AM",
-        reservedby: "Irina Solarez"
-      }
-    ]
+    seat: "A02"
   },
   {
-    seat: "A03",
-    reservations: [
-      {
-        name: "Andres Hidalgo",
-        value: 1,
-        anonymous: 0,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-07-31",
-        timeofrequest: "8:00 AM",
-        timeofreservation: "9:30AM-10:00AM",
-        reservedby: "Andres Hidalgo"
-      }
-    ]
+    seat: "A03"
   },
   {
-    seat: "A04",
-    reservations: [
-      {
-        name: "Stephen Go",
-        value: 1,
-        anonymous: 1,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-07-31",
-        timeofrequest: "8:00 AM",
-        timeofreservation: "9:30AM-10:00AM",
-        reservedby: "Stephen Go"
-      }
-    ]
+    seat: "A04"
   },
   {
-    seat: "A05",
-    reservations: [
-      {
-        name: "Japeth Aguilar",
-        value: 1,
-        anonymous: 1,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-07-31",
-        timeofrequest: "8:00 AM",
-        timeofreservation: "9:30AM-10:00AM",
-        reservedby: "Japeth Aguilar"
-      }
-    ]
+    seat: "A05"
   },
   {
-    seat: "A06",
-    reservations: [
-      {
-        name: "Marie Espiritu",
-        value: 1,
-        anonymous: 1,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-07-31",
-        timeofrequest: "10:00 AM",
-        timeofreservation: "10:00AM-10:30AM",
-        reservedby: "Marie Espiritu"
-      }
-    ]
+    seat: "A06"
   },
   {
-    seat: "A07",
-    reservations: [
-      {
-        name: "Claudette Escobar",
-        value: 1,
-        anonymous: 1,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-07-31",
-        timeofrequest: "8:00 AM",
-        timeofreservation: "10:00AM-10:30AM",
-        reservedby: "Claudette Escobar"
-      }
-    ]
+    seat: "A07"
   },
   {
-    seat: "A08",
-    reservations: [
-      {
-        name: "Mari Santos",
-        value: 1,
-        anonymous: 0,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-08-03",
-        timeofrequest: "8:00 AM",
-        timeofreservation: "7:30AM-8:00AM",
-        reservedby: "Mari Santos"
-      }
-    ]
+    seat: "A08"
   },
   {
-    seat: "A09",
-    reservations: [
-      {
-        name: "Carlos Bautista",
-        value: 1,
-        anonymous: 0,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-08-03",
-        timeofrequest: "9:00 AM",
-        timeofreservation: "7:30AM-8:00AM",
-        reservedby: "Carlos Bautista"
-      }
-    ]
+    seat: "A09"
   },
   {
-    seat: "A10",
-    reservations: [
-      {
-        name: "Andres Hidalgo",
-        value: 1,
-        anonymous: 0,
-        dateofrequest: "2024-07-31",
-        dateofreservation: "2024-08-03",
-        timeofrequest: "8:00 AM",
-        timeofreservation: "9:30AM-10:00AM",
-        reservedby: "Andres Hidalgo"
-      }
-    ]
+    seat: "A10"
   },
   {
     seat: "A11"
